15899e6cfc0b472f4c3e9247fbf79021bc07454a
abc222 <1569433461@qq.com>
2019-04-28 15:19:48 +0800

    印第安人糖尿病诊断多层感知器实现可视化训练过程

--- pima_indians/PimabyMultilayerSensor.py
+++ pima_indians/PimabyMultilayerSensor.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from matplotlib import pyplot as plt
 import numpy as np
 
 # 设定随机数种子
@@ -21,7 +22,7 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # 训练模型
-model.fit(x=x, y=Y, epochs=150, batch_size=10)
+history = model.fit(x=x, y=Y, epochs=150, batch_size=10, validation_split=0.2)
 
 # 评估模型
 scores = model.evaluate(x=x, y=Y)
@@ -32,3 +33,23 @@ print('\n%s : %.2f%%' % (model.metrics_names[1], scores[1] * 100))
 # # round predictions
 # rounded = [round(x) for x in predictions]
 # print(rounded)
+
+print(history.history.keys())
+
+# accuracy的历史
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+# loss的历史
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
